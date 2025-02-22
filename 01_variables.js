da9d6ba85ce7d152616eeedf631f6e74ff40797a
Mayank Upadhyay <155510307+Mayankupadhyay-25@users.noreply.github.com>
2025-02-22 16:01:02 +0530

    datatype

---
+++ 01_variables.js
@@ -0,0 +1,21 @@
+const accountID = 112235
+ let accountemail ="mayankupadhayay1125@gmail.com"
+ var accountpassword = "123"
+ accountcity ="prayagraj"
+
+ //accountID = 2 //not allowed
+ accountemail ="maya@,com"
+ accountpassword="134335"
+ accountcity="varanasi"
+ let accountState ;
+
+/*
+prefer not to use var
+because of issue in block scope and functionl scope
+*/
+
+ console.log(accountID);
+
+ console.table([accountID,accountemail,accountpassword,accountcity,accountState]);
+ 
+ 
