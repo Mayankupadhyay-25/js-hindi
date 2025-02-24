275bd0cfd4e0403c870285ed9b18c854e6f4fd16
Mayank Upadhyay <155510307+Mayankupadhyay-25@users.noreply.github.com>
2025-02-25 00:02:48 +0530

    comparison

---
+++ Comparison.js
@@ -0,0 +1,22 @@
+// console.log( 2 > 1);
+// console.log( 2 < 1);
+// console.log( 2 == 1);
+// console.log( 2 != 1);
+
+// it can't allow to compaier two differnt data type
+
+// console.log("2" > 1);
+// console.log( "02" > 1);
+
+// under this conversion always gives confusion
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined = 0);
+console.log(undefined < 0);
+console.log(undefined > 0);
+
+// === strick check
+
+console.log("2" === 2);
