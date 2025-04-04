6340065993a0e49c44b3cf580100c11840c42f6c
Mayank Upadhyay <155510307+Mayankupadhyay-25@users.noreply.github.com>
2025-04-04 12:03:16 +0530

    string

---
+++ string.js
@@ -0,0 +1,10 @@
+const name = "Mayank"
+const repocount = 50
+
+// console.log(name + repocount + "value");
+
+console.log(`hello my name is ${name} and my repo count is 
+    ${repocount}`); // this is morden way of syntex it's known as (string interpolation)
+    
+    // another way of declearing string is:
+    const gameName = new String('mayankkk')    
