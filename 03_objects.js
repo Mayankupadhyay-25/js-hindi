3e9cfe0f04709df89a2d77492f07996cc7f02203
Mayank Upadhyay <155510307+Mayankupadhyay-25@users.noreply.github.com>
2025-04-13 11:07:48 +0530

    object03

---
+++ 03_objects.js
@@ -0,0 +1,35 @@
+//singleton
+
+//object literals
+
+const mySym = Symbol("key1")
+const JsUser ={
+name: "mayank",
+"full name": "mayank Upadhyay",
+[mySym]: "myKey1",
+age: 21,
+location: "delhi",
+hobbies: ["gaming", "reading"],     
+email: "mayank@gmail.com",
+isLoggedIn: false,
+isLogginDays: ["Monday" , "Saturday"]
+}
+
+// console.log(JsUser.email) // Accessing object property
+// console.log(JsUser["email"]) 
+// console.log(JsUser["full name"]) 
+// console.log(typeof JsUser[mySym]) // undefined
+// console.log(JsUser) // undefined;
+
+JsUser.email = "mayank@chatgpt.com" // Accessing object property
+//Object.freeze(JsUser)
+JsUser.email = "mayank@microsoft.com"
+console.log(JsUser)
+
+JsUser.greeting = function(){   
+    console.log("hello mayank")
+}
+
+console.log(JsUser.greeting) // hello mayank;
+console.log(JsUser.greeting()) 
+
