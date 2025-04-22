89862c8be218a6af2f2e7be9d5b3c9f7d688bc5b
Mayank Upadhyay <155510307+Mayankupadhyay-25@users.noreply.github.com>
2025-04-22 16:12:24 +0530

    basic_array

--- 03_basic/03_basic_Array.js
+++ 03_basic/03_basic_Array.js
@@ -1,4 +1,33 @@
 const user ={
     name: "mayank",
-    price
+    price : 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username}, welcome to wensite`);
+    }
+
 }
+
+// user.welcomeMessage() // undefined 
+// user.username = "satyam "
+// user.welcomeMessage() // undefined
+// console.log(this);
+
+// const chai = () => {
+//     let username = "mayank"
+//     console.log(this);
+// }
+
+//   chai() // undefined
+
+//   const addTwo = (num1, num2) =>{
+//     return num1 + num2
+//   }
+
+
+// const addTwo = (num1, num2) =>  num1 + num2
+
+const addTwo = (num1, num2) =>  {username:"mayank"}
+    
+  
+  console.log(addTwo(2,3)); // 5
