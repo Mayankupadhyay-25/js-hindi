3c02a8b2e710aaadc8978608a54c2ad6e35ddadf
Mayank Upadhyay <155510307+Mayankupadhyay-25@users.noreply.github.com>
2025-04-24 00:33:57 +0530

    IIFE

---
+++ 03_basic/04_iife.js
@@ -0,0 +1,10 @@
+
+// IIFE - Immediately Invoked Function Expression
+
+(function chai() {
+    console.log(`DB CONNCETED`);
+})();
+
+( (name) => {
+    console.log(`DB CONNCETED TWO ${name}`);
+    })('mayank') // IIFE - Immediately Invoked Function Expression
